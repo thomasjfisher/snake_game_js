75a526ec9e26b74fcf7d26d0140e74f596141158
THOMAS <thomasjfisher1@gmail.com>
2020-10-04 11:49:50 +1100

    finished creating snake game.

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
 // Creating a Snake Game via video below
 // https://www.youtube.com/watch?v=QTcIXok9wNY&t=813s
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood }  from './food.js'
+import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 
 function main(currentTime) {
+    if (gameOver) {
+        if (confirm('You Lost! Press ok to restart!')){
+            window.location = '/'
+        }
+        return 
+    }
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
@@ -23,6 +31,7 @@ window.requestAnimationFrame(main)
 function update() {
     updateSnake()
     updateFood()
+    checkForDeath()
 }
 
 function draw() {
@@ -31,3 +40,6 @@ function draw() {
     drawFood(gameBoard)
 }
 
+function checkForDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
